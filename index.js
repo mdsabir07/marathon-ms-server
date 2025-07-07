63425b54259ff36431bdfae6d66955b5a6f47be9
mdsabir07 <sabiruli@gmail.com>
2025-07-07 23:26:59 +0600

    Create server side using express and intasll express, cors, dotenv, mongodb

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000;
 const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
 
 
 app.get('/', (req, res) => {
